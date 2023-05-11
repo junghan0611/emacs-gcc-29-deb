b735a2e3ab787192dc53d39c9d7e29e885ac1ee7
junghan0611 <junghanacs@gmail.com>
2023-05-11 18:03:36 +0900

    fix

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env sh
-# docker build --file Dockerfile
-#  docker build -t konstare/emacs-gcc-pgtk:latest .
-# id=$(docker create anacs/emacs-gcc-pgtk)
+# docker build -t konstare/emacs-gcc-pgtk:latest .
+# id=$(docker create junghanacs/emacs-gcc-pgtk)
 # docker cp $id:/opt/deploy .
 
 # 여기 보고 한다.
 # https://github.com/Riey/kime
 sudo docker build --file Dockerfile --tag emacs-gcc-29:git .
+sudo docker run --name emacs-gcc-29 emacs-gcc-29:git
+sudo docker cp emacs-gcc-29:/opt/deploy .
 
+# 72M emacs-gcc-gtk_29.0.90.23.05.11.08.deb
 
 # docker build --file build-docker/<distro path>/Dockerfile --tag kime-build:git .
 # docker run --name kime kime-build:git
